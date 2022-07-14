60f337b0d89e63e2b8369854e5f5f8634c0acbaf
xiaoyang007 <925601810@qq.com>
2022-07-15 07:31:52 +0800

    更新

--- pages/index/classify.vue
+++ pages/index/classify.vue
@@ -1,6 +1,5 @@
 <template>
 	<view class="page-warp overflow">
-
 		<view class="top-warp">
 			<!-- <view class="">
 				<tm-search :bgColor="$tm.vx.state().user.themeColor" :insertColor="$tm.vx.state().user.themeColor"
@@ -12,17 +11,40 @@
 
 		<view class="center-warp">
 			<!-- 左边 -->
-			<tm-sliderNav :round="0" :color="$tm.vx.state().user.themeColor" textOverflow="text-overflow"
-				:width="180" @change="change" rang-key="categoryName" :list="tabsGoods" text bg-color="grey-lighten-3">
-			</tm-sliderNav>
+			<tm-sliderNav
+				:round="0"
+				:color="$tm.vx.state().user.themeColor"
+				textOverflow="text-overflow"
+				:width="170"
+				@change="change"
+				rang-key="categoryName"
+				:list="tabsGoods"
+				text
+				bg-color="grey-lighten-3"
+			></tm-sliderNav>
 
-			<view class="right-warp">
-				<tm-tabs :color="$tm.vx.state().user.themeColor" v-model="tabIndex" :list="tabsGoods[tabIndex].children" align="left"
-					range-key="categoryName" :active-border-color="$tm.vx.state().user.themeColor"></tm-tabs>
+			<view class="right-warp white">
+				<tm-tabs
+					v-model="twoIndex"
+					:color="$tm.vx.state().user.themeColor"
+					:list="tabsGoods[tabIndex].children"
+					align="left"
+					range-key="categoryName"
+					:active-border-color="$tm.vx.state().user.themeColor"
+				></tm-tabs>
 				<!--右边 :fixed="false", 高度跟随父元素 (不在组件上定义class,避免部分小程序平台编译丢失, 如支付宝,钉钉小程序) -->
-				<mescroll-uni :fixed="false" ref="mescrollRef" @init="mescrollInit" @down="downCallback"
-					@up="upCallback">
-
+				<mescroll-uni :fixed="false" ref="mescrollRef" @init="mescrollInit" @down="downCallback" @up="upCallback">
+					<view v-for="(v, i) in 1" :key="i" class="flex mx-30 py-30 border-b-1">
+						<view class="round-2 " style="height: 135rpx; width: 135rpx;"><tm-images :round="2" src="https://picsum.photos/300?id=7 "></tm-images></view>
+						<view class="flex-1 ml-10 flex-column">
+							<view class="text-overflow-2	text-size-n text-weight-b">产品名称</view>
+							<view class="flex-1">但是</view>
+							<view class="flex-between">
+								<view class="text-size-g text-red mt-25 text-weight-b">¥12</view>
+								<tm-stepper circular v-model="word4" :min="-10" :max="10" :step="1" :round="24" color="bg-gradient-pink-accent "></tm-stepper>
+							</view>
+						</view>
+					</view>
 				</mescroll-uni>
 			</view>
 		</view>
@@ -30,26 +52,32 @@
 </template>
 
 <script>
-	import MescrollMixin from "@/uni_modules/mescroll-uni/components/mescroll-uni/mescroll-mixins.js";
-	export default {
-		mixins: [MescrollMixin], // 使用mixin
-		data() {
-			return {
-				tabsGoods: [],
-				tabIndex: 0 // tab下标
-			}
-		},
-		methods: {
-			upCallback(page) {
-				
-				// tabs异步获取
-				const {clientId, shopId} = uni.$tm.vx.state().user.userInfo
-				uni.$http.get('/wx/goods/getCategoryGoodsList', {clientId: clientId, shopId: shopId }, {that: this}).then(res => {
-					console.log(res)
-					this.tabsGoods = res;
-				}).catch(() => {})
-				this.mescroll.endSuccess(0);
-				/* if(this.tabs.length == 0){
+import MescrollMixin from '@/uni_modules/mescroll-uni/components/mescroll-uni/mescroll-mixins.js';
+export default {
+	mixins: [MescrollMixin], // 使用mixin
+	data() {
+		return {
+			tabsGoods: [],
+			tabIndex: 0, // tab下标
+			twoIndex: 0,
+			word4: 0
+		};
+	},
+	methods: {
+		upCallback(page) {
+			// tabs异步获取
+			this.mescroll.endSuccess(0);
+			const { clientId, shopId } = uni.$tm.vx.state().user.userInfo;
+			// uni.$http
+			// 	.get('/wx/goods/getCategoryGoodsList', { clientId: 26, shopId: 16 })
+			// 	.then(res => {
+			// 		console.log(res);
+			// 		this.mescroll.endSuccess(res.length);
+			// 		this.tabsGoods = res;
+			// 	})
+			// 	.catch(() => {});
+
+			/* if(this.tabs.length == 0){
 					apiGetTabs().then(res=>{
 						this.tabs = res
 						this.mescroll.resetUpScroll() // 重新触发upCallback
@@ -71,62 +99,63 @@
 					//联网失败, 结束加载
 					this.mescroll.endErr();
 				}) */
-			},
-			// 切换菜单
-			tabChange(i) {
-				if (this.tabIndex != i) {
-					this.tabIndex = i
-					this.goods = []; // 先置空列表,显示加载进度条
-					this.mescroll.resetUpScroll(); // 重置列表数据
-				}
-			},
-			change(e) {
-				console.log(e)
+		},
+		// 切换菜单
+		tabChange(i) {
+			if (this.tabIndex != i) {
+				this.tabIndex = i;
+				this.goods = []; // 先置空列表,显示加载进度条
+				this.mescroll.resetUpScroll(); // 重置列表数据
 			}
+		},
+		change(index) {
+			this.twoIndex = 0;
+			this.tabIndex = index;
 		}
 	}
+};
 </script>
 
 <style lang="scss">
-	/*需给父元素设置height:100%*/
-	.page-warp {
-		height: 100%;
+/*需给父元素设置height:100%*/
+.page-warp {
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+
+	/* 顶部区域 */
+	.top-warp {
+		text-align: center;
+	}
+
+	/* 中间 */
+	.center-warp {
+		flex: 1;
+		min-width: 0;
+		min-height: 0;
+		/* 需给flex:1的元素加上最小高,否则内容超过会溢出容器 (如:小程序Android真机) */
 		display: flex;
-		flex-direction: column;
 
-		/* 顶部区域 */
-		.top-warp {
-			text-align: center;
-		}
+		// 左边
+		.left-warp {
+			width: 180rpx;
+			height: 100%;
 
-		/* 中间 */
-		.center-warp {
-			flex: 1;
-			min-width: 0;
-			min-height: 0;
-			/* 需给flex:1的元素加上最小高,否则内容超过会溢出容器 (如:小程序Android真机) */
-			display: flex;
+			.tab {
+				font-size: 28rpx;
+				padding: 28rpx;
 
-			// 左边
-			.left-warp {
-				width: 180rpx;
-				height: 100%;
-
-				.tab {
-					font-size: 28rpx;
-					padding: 28rpx;
-
-					&.active {
-						background-color: #fff;
-					}
+				&.active {
+					background-color: #fff;
 				}
 			}
+		}
 
-			// 右边
-			.right-warp {
-				flex: 1;
-				min-width: 0;
-			}
+		// 右边
+		.right-warp {
+			flex: 1;
+			min-width: 0;
 		}
 	}
+}
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -8,80 +8,74 @@
 			</template>
 		</tm-menubars>
 
-		<scroll-view class="center_warp" scroll-y :style="{height: bodyHeight + 'px'}">
+		<scroll-view class="center_warp" scroll-y :style="{ height: bodyHeight + 'px' }">
 			<home v-if="tabIndex == 0" class="fulled-height"></home>
 			<classify v-else-if="tabIndex == 1" class="fulled-height"></classify>
 			<cart v-else-if="tabIndex == 2" class="fulled-height" :bottom="cardBar"></cart>
 			<my v-else-if="tabIndex == 3" class="fulled-height"></my>
 		</scroll-view>
 
-		<tm-bottomnavigation class="tabbar" :icon-color="$tm.vx.state().user.themeColor"
-			icon-color-grey="grey-lighten-1" :list="tabbar" @change="tabBarChange"></tm-bottomnavigation>
+		<tm-bottomnavigation class="tabbar" :icon-color="$tm.vx.state().user.themeColor" icon-color-grey="grey-lighten-1" :list="tabbar" @change="tabBarChange"></tm-bottomnavigation>
 	</view>
 </template>
 
 <script>
-	import Home from './home.vue'
-	import Classify from './classify.vue'
-	import Cart from './cart.vue'
-	import My from './my.vue'
-	export default {
-		components: {
-			Home,
-			Classify,
-			Cart,
-			My
-		},
-		data() {
-			return {
-				bodyHeight: '100%',
-				cardBar: 0,
-				menuTitle: '首页',
-				tabIndex: 3,
-				tabbar: [{
-						icon: 'icon-position-fill',
-						value: '首页',
-						fontSize: 25
-					},
-					{
-						icon: 'icon-smile-fill',
-						value: '分类',
-						fontSize: 25
-					},
-					{
-						icon: 'icon-clock-fill',
-						value: '购物车',
-						fontSize: 25
-					},
-					{
-						icon: 'icon-times',
-						value: '我的',
-						fontSize: 25
-					}
-				],
-			};
-		},
-		created() {
-			let sys = uni.getSystemInfoSync();
-			this.cardBar = ((sys.screenHeight - sys.safeArea.bottom) + uni.upx2px(52 + 46));
-			this.bodyHeight = (sys.screenHeight - ((sys.screenHeight - sys.safeArea.bottom) + (sys.statusBarHeight + 45)) -
-				uni.upx2px(52 + 46));
-		},
-		onLoad(option) {
-		},
-		onShow() {
-		},
-		methods: {
-			// 选中tab回调事件
-			tabBarChange: uni.$tm.throttle(function(e) {
-				this.tabIndex = e.index;
-				this.menuTitle = e.item.value;
-			}),
-			
-		}
-	};
+import Home from './home.vue';
+import Classify from './classify.vue';
+import Cart from './cart.vue';
+import My from './my.vue';
+export default {
+	components: {
+		Home,
+		Classify,
+		Cart,
+		My
+	},
+	data() {
+		return {
+			bodyHeight: '100%',
+			cardBar: 0,
+			menuTitle: '首页',
+			tabIndex: 1,
+			tabbar: [
+				{
+					icon: 'icon-position-fill',
+					value: '首页',
+					fontSize: 25
+				},
+				{
+					icon: 'icon-smile-fill',
+					value: '分类',
+					fontSize: 25
+				},
+				{
+					icon: 'icon-clock-fill',
+					value: '购物车',
+					fontSize: 25
+				},
+				{
+					icon: 'icon-times',
+					value: '我的',
+					fontSize: 25
+				}
+			]
+		};
+	},
+	created() {
+		let sys = uni.getSystemInfoSync();
+		this.cardBar = sys.screenHeight - sys.safeArea.bottom + uni.upx2px(52 + 46);
+		this.bodyHeight = sys.screenHeight - (sys.screenHeight - sys.safeArea.bottom + (sys.statusBarHeight + 45)) - uni.upx2px(52 + 46);
+	},
+	onLoad(option) {},
+	onShow() {},
+	methods: {
+		// 选中tab回调事件
+		tabBarChange: uni.$tm.throttle(function(e) {
+			this.tabIndex = e.index;
+			this.menuTitle = e.item.value;
+		})
+	}
+};
 </script>
-<style>
-</style>
-<style lang="scss" scoped>
-</style>
+<style></style>
+<style lang="scss" scoped></style>
